49f25d3ceeb9dc69d84c3d19c57d3a3e8bde3a8b
Jack Henza <jackhenza@gmail.com>
2024-11-18 15:21:36 +0000

    chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -41,6 +41,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
